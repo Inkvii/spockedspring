15f43bf5073ec9c73a9e9502e44b8bd7c7b45e6f
Lubos Verzich <lve@paymorrow.com>
2021-05-16 12:28:16 +0200

    Functional springboot test on kotest with mocked repositories

--- src/test/kotlin/verzich/spockedspring/petshop/PetshopServiceSpringboottest.kt
+++ src/test/kotlin/verzich/spockedspring/petshop/PetshopServiceSpringboottest.kt
@@ -1,44 +1,64 @@
 package verzich.spockedspring.petshop
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.test.TestCase
 import io.kotest.matchers.shouldNotBe
 import io.mockk.every
+import io.mockk.mockk
 import io.mockk.verifyAll
 import org.springframework.boot.test.context.SpringBootTest
 import verzich.spockedspring.human.HumanEntity
+import verzich.spockedspring.human.HumanRepository
 import verzich.spockedspring.pet.Cat
+import verzich.spockedspring.pet.PetRepository
 import java.math.BigDecimal
 import java.util.*
 
 
 @SpringBootTest
-class PetshopServiceSpringboottest(val petshopService: PetshopService) : FunSpec({
+class PetshopServiceSpringboottest(
+    val petshopRepository: PetshopRepository,
+    val petshopAdministrationService: PetshopAdministrationService
+) : FunSpec() {
 
+    // test subject - if we need to mock something, we have to declare this manually.
+    lateinit var petshopService: PetshopService
 
-    test("Buy pet") {
+    // mocked repositories because otherwise it would interact with database
+    val humanRepository: HumanRepository = mockk()
+    val petRepository: PetRepository = mockk()
 
-        val request = PetshopController.BuyPetRequest(1L, 1L)
+    // do your own autowiring for the service
+    override fun beforeTest(testCase: TestCase) {
+        petshopService = PetshopService(petshopRepository, petRepository, humanRepository, petshopAdministrationService)
+    }
 
-        val humanEntity = HumanEntity("Alois", BigDecimal("1000.12"), mutableListOf())
-        humanEntity.id = 1L
+    init {
+        test("Buy pet") {
 
-        val cat = Cat("name", 1, BigDecimal.ONE, BigDecimal.ONE, null, PetshopEntity("name", mutableListOf(), BigDecimal.ONE))
-        cat.id = 1L
+            val request = PetshopController.BuyPetRequest(1L, 1L)
 
-        every { petshopService.humanRepository.findById(request.humanId) } returns Optional.of(humanEntity)
-        every { petshopService.petRepository.findByIdAndOwnerIsNull(request.chosenPetId) } returns Optional.of(cat)
+            val humanEntity = HumanEntity("Alois", BigDecimal("1000.12"), mutableListOf())
+            humanEntity.id = 1L
 
-        // when
-        val response = petshopService.buyPet(request)
+            val cat = Cat("name", 1, BigDecimal.ONE, BigDecimal.ONE, null, PetshopEntity("name", mutableListOf(), BigDecimal.ONE))
+            cat.id = 1L
 
-        // then
-        verifyAll {
-            petshopService.petRepository.findByIdAndOwnerIsNull(request.chosenPetId)
-            petshopService.humanRepository.findById(request.humanId)
-            petshopService.petshopAdministrationService.checkHumanCanAffordBuyingPet(humanEntity, cat)
-            petshopService.petshopAdministrationService.checkHumanCanAffordFeedingAllPets(humanEntity, cat)
-            petshopService.petshopAdministrationService.transferOwnershipToHuman(humanEntity, cat)
-            response shouldNotBe null
+            every { humanRepository.findById(request.humanId) } returns Optional.of(humanEntity)
+            every { petRepository.findByIdAndOwnerIsNull(request.chosenPetId) } returns Optional.of(cat)
+
+            // when
+            val response = petshopService.buyPet(request)
+
+            // then
+            verifyAll {
+                petshopService.petRepository.findByIdAndOwnerIsNull(request.chosenPetId)
+                humanRepository.findById(request.humanId)
+                petshopService.petshopAdministrationService.checkHumanCanAffordBuyingPet(humanEntity, cat)
+                petshopService.petshopAdministrationService.checkHumanCanAffordFeedingAllPets(humanEntity, cat)
+                petshopService.petshopAdministrationService.transferOwnershipToHuman(humanEntity, cat)
+                response shouldNotBe null
+            }
         }
     }
-})
+}
