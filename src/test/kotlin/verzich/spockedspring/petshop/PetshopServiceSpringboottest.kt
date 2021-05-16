c24abd1bcf74120093495ae265059d21a2a20810
Lubos Verzich <lve@paymorrow.com>
2021-05-16 12:38:20 +0200

    Removed init

--- src/test/kotlin/verzich/spockedspring/petshop/PetshopServiceSpringboottest.kt
+++ src/test/kotlin/verzich/spockedspring/petshop/PetshopServiceSpringboottest.kt
@@ -1,8 +1,8 @@
 package verzich.spockedspring.petshop
 
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.core.test.TestCase
 import io.kotest.matchers.shouldNotBe
+import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verifyAll
@@ -19,7 +19,7 @@ import java.util.*
 class PetshopServiceSpringboottest(
     val petshopRepository: PetshopRepository,
     val petshopAdministrationService: PetshopAdministrationService
-) : FunSpec() {
+) : FunSpec({
 
     // test subject - if we need to mock something, we have to declare this manually.
     lateinit var petshopService: PetshopService
@@ -29,36 +29,39 @@ class PetshopServiceSpringboottest(
     val petRepository: PetRepository = mockk()
 
     // do your own autowiring for the service
-    override fun beforeTest(testCase: TestCase) {
+    beforeTest {
         petshopService = PetshopService(petshopRepository, petRepository, humanRepository, petshopAdministrationService)
     }
+    afterTest {
+        clearAllMocks()
+    }
 
-    init {
-        test("Buy pet") {
 
-            val request = PetshopController.BuyPetRequest(1L, 1L)
+    test("Buy pet") {
 
-            val humanEntity = HumanEntity("Alois", BigDecimal("1000.12"), mutableListOf())
-            humanEntity.id = 1L
+        val request = PetshopController.BuyPetRequest(1L, 1L)
 
-            val cat = Cat("name", 1, BigDecimal.ONE, BigDecimal.ONE, null, PetshopEntity("name", mutableListOf(), BigDecimal.ONE))
-            cat.id = 1L
+        val humanEntity = HumanEntity("Alois", BigDecimal("1000.12"), mutableListOf())
+        humanEntity.id = 1L
 
-            every { humanRepository.findById(request.humanId) } returns Optional.of(humanEntity)
-            every { petRepository.findByIdAndOwnerIsNull(request.chosenPetId) } returns Optional.of(cat)
+        val cat = Cat("name", 1, BigDecimal.ONE, BigDecimal.ONE, null, PetshopEntity("name", mutableListOf(), BigDecimal.ONE))
+        cat.id = 1L
 
-            // when
-            val response = petshopService.buyPet(request)
+        every { humanRepository.findById(request.humanId) } returns Optional.of(humanEntity)
+        every { petRepository.findByIdAndOwnerIsNull(request.chosenPetId) } returns Optional.of(cat)
 
-            // then
-            verifyAll {
-                petshopService.petRepository.findByIdAndOwnerIsNull(request.chosenPetId)
-                humanRepository.findById(request.humanId)
-                petshopService.petshopAdministrationService.checkHumanCanAffordBuyingPet(humanEntity, cat)
-                petshopService.petshopAdministrationService.checkHumanCanAffordFeedingAllPets(humanEntity, cat)
-                petshopService.petshopAdministrationService.transferOwnershipToHuman(humanEntity, cat)
-                response shouldNotBe null
-            }
+        // when
+        val response = petshopService.buyPet(request)
+
+        // then
+        verifyAll {
+            petshopService.petRepository.findByIdAndOwnerIsNull(request.chosenPetId)
+            humanRepository.findById(request.humanId)
+            petshopService.petshopAdministrationService.checkHumanCanAffordBuyingPet(humanEntity, cat)
+            petshopService.petshopAdministrationService.checkHumanCanAffordFeedingAllPets(humanEntity, cat)
+            petshopService.petshopAdministrationService.transferOwnershipToHuman(humanEntity, cat)
+            response shouldNotBe null
         }
     }
-}
+
+})
